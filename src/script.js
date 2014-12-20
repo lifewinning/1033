b3453fe3331df870b63ce1c1cd1f7996df615f5a
lifewinning <lifewinning@gmail.com>
2014-12-19 16:21:48 -0800

    1033 1033

--- src/script.js
+++ src/script.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
-	$('#content').jscroll({
-		loadingHtml: '<small>(There are a lot of things, like 193,176 of them, so it might take a while to load. They are totally loading though.)',
-		padding: 20
-	});
-
 	$.getJSON("./data/allstates.json", function(data){
-		for (var i = 0; i < data.length; i++) {
+		$('#content').jscroll({
+		loadingHtml: '',
+		padding: 0
+		});
+
+		function get1033(arr, count) {
+		    var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+		    while (i-- > min) {
+		        index = Math.floor((i + 1) * Math.random());
+		        temp = shuffled[index];
+		        shuffled[index] = shuffled[i];
+		        shuffled[i] = temp;
+		    }
+		    return shuffled.slice(min);
+		}
+
+		var the1033 = get1033(data, 1033);
+
+		for (var i = 0; i < the1033.length; i++) {
 		// var google = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + data[i].item_name_edited+'&imgsz=large';
 		// $.getJSON(google, function(image){
 		// 	console.log(image);
@@ -13,16 +26,16 @@ $(document).ready(function(){
 		 //make a div
 		 
 		  var thisDiv = document.createElement('div');
-		  thisDiv.ID = data[i].NSN;
+		  thisDiv.ID = the1033[i].NSN;
 		  thisDiv.className = "col-md-3";
 		  thisDiv.style.minHeight = "250px";
 		  var innerdiv = document.createElement('div');
 		  innerdiv.className = "innerdiv"
 		  var objID = document.createElement('h3');
-		  objID.innerHTML = data[i].item_name_edited + ' ('+data[i].quantity+')';
+		  objID.innerHTML = the1033[i].item_name_edited + ' ('+the1033[i].quantity+')';
 
 		  var agency = document.createElement('p');
-		  agency.innerHTML = data[i].agency_name +", "+data[i].state+", "+data[i].year;
+		  agency.innerHTML = the1033[i].agency_name +", "+the1033[i].state+", "+the1033[i].year;
 
 		  $(innerdiv).append(objID);
 		  $(innerdiv).append(agency);
@@ -30,6 +43,7 @@ $(document).ready(function(){
 		  $('#content').append(thisDiv);
 		}
 	});
+
 
 });
 
